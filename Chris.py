f09800b01d3e32a768c22a7f8c781256cdc9fe9c
roberto <roberto.rodriguez@calbaptist.edu>
2021-03-01 17:58:46 -0800

    database up

--- Chris.py
+++ Chris.py
@@ -1,2 +1,14 @@
-#This is a changed file
-#this is a change
+from titoooooooooo import DB
+
+"""
+Driver for creating the database, showing rows, and editing it
+"""
+def main():
+    print("DBAPI Lab starting up...")
+    print()
+    db = DB()
+    db.create_db()
+    db.insert_base_data()
+
+if __name__ == '__main__':
+    main()
