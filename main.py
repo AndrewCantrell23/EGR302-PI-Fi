f371ce3a502d0ac0c4fbf3baf22a4b0bec1ab3c7
Andrew Cantrell <andyroocantrell@gmail.com>
2021-03-03 13:55:54 -0800

    Added json file reading capability into main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import os
 
 os.system('python speedtest-cli.py --json > myoutput.json')
+
+f = open("myoutput.json", "r")
+
+# read from json file
+line = f.readline()
+arr = line.split()
+
+# assign speed variables
+download = (str(arr[1]))[0:(len(arr[1]) - 1)]
+upload = (str(arr[3]))[0:(len(str(arr[3])) - 1)]
+ping = (str(arr[5]))[0:(len(str(arr[5])) - 1)]
+
+# for debugging purposes currently
+print(download)
+print(upload)
+print(ping)
+
+#TODO add sql connect server and execute insert command
