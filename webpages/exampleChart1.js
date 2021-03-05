0965199b792c4c3e52f921c23d00f4560a10ebcb
ReuellaJacob <reuella.jacob@calbaptist.edu>
2021-03-05 14:32:05 -0800

    added an example chart

--- webpages/exampleChart1.js
+++ webpages/exampleChart1.js
@@ -1,26 +1,26 @@
 var ctx = document.getElementById('exampleChart1');
-var frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
-
 var exampleChart1 = new Chart(ctx, {
         type: 'bar',
         data: {
-            label: frameworks,
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
-                label: 'Test1',
-                data: [135850, 52122, 148825, 16939, 9763],
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
                 backgroundColor: [
-                    "rgba(255, 99, 132, 0.2)",
-                    "rgba(54, 162, 235, 0.2)",
-                    "rgba(255, 206, 86, 0.2)",
-                    "rgba(75, 192, 192, 0.2)",
-                    "rgba(153, 102, 255, 0.2)"
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
                 ],
                 borderColor: [
-                    "rgba(255, 99, 132, 1)",
-                    "rgba(54, 162, 235, 1)",
-                    "rgba(255, 206, 86, 1)",
-                    "rgba(75, 192, 192, 1)",
-                    "rgba(153, 102, 255, 1)",
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
                 ],
                 borderWidth: 1
             }]
@@ -28,6 +28,13 @@ var exampleChart1 = new Chart(ctx, {
         options: {
             maintainAspectRatio: false,
             responsive: false,
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
         }
     }
 );
