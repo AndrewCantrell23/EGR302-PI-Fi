6dcbc7a07b13b9471c5acf6c71137dbad0f0c0de
ReuellaJacob <reuella.jacob@calbaptist.edu>
2021-03-12 11:48:41 -0800

    Figured out that DB time and Speedtest-cli time do not match.

--- main.py
+++ main.py
@@ -5,20 +5,20 @@ import json
 
 def main():
     base = DB()
-    # os.system('python speedtest-cli.py --json > myoutput.json')
-    #
-    # with open('myoutput.json') as f:
-    #     unfiltered = json.load(f)
+    os.system('python speedtest-cli.py --json > myoutput.json')
+
+    with open('myoutput.json') as f:
+        unfiltered = json.load(f)
 
     # TODO: Download and Upload values are too big for the database requirement
-    # data = {
-    #     'Location': 'Lancer Arms',
-    #     'Download': unfiltered['download'],
-    #     'Ping': unfiltered['ping'],
-    #     'Upload': unfiltered['upload'],
-    #     'times': unfiltered['timestamp']
-    #
-    # }
+    data = {
+        'Location': 'Lancer Arms',
+        'Download': unfiltered['download']/1000000,
+        'Ping': unfiltered['ping'],
+        'Upload': unfiltered['upload']/1000000,
+        'times': unfiltered['timestamp']
+
+    }
 
     # want is useless
     want = {  # this here should be the json file
@@ -27,8 +27,13 @@ def main():
         'Download': 152.0000,
         'Upload': 32.90009
     }
-    base.enter_data(data=want)
+    base.enter_data(data=data)
     # results stores the query results as a dataFrame object
+    for item in data:
+        print(f"{item}: {data[item]}")
+
+    print("DATABASE STUFF")
+
     results = base.extract(cols='*')
     print(results)
 
