9dc9383c7dd75d1c9e526201cbdb8661227a1df9
reuellajacob <reuella.jacob@calbaptist.edu>
2021-04-20 01:24:09 -0700

    added main for point

--- main_point.py
+++ main_point.py
@@ -30,7 +30,7 @@ def job():
         unfiltered = json.load(f)
 
     data = {
-        'Location': 'Lancer Arms',
+        'Location': 'Point',
         'Ping': unfiltered['ping'],
         'Download': unfiltered['download'] / 1000000,
         'Upload': unfiltered['upload'] / 1000000,
