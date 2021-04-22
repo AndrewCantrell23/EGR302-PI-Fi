2e8a5b638e2d5cf9fb5f3396f4f3612245c652a3
roberto <roberto.rodriguez@calbaptist.edu>
2021-04-22 02:07:29 -0700

    fixed database automation

---
+++ main.py
@@ -0,0 +1,50 @@
+import os
+from DatabaseConnection import DB
+import json
+from datetime import datetime
+import pytz
+from schedule import every, repeat, run_pending
+import time
+
+
+def change_timezone_to_pst(utc_datetime_object):
+    pst = pytz.timezone("Us/Pacific")
+    return utc_datetime_object.astimezone(pst).strftime('%D %I:%M:%S %p')
+
+
+def replace_datetime_formats(parsed_list):
+    for speedtest_object in parsed_list:
+        for dictPair in speedtest_object:
+            if dictPair == "Times":
+                datetime_object = datetime.strptime(speedtest_object.get(dictPair), "%Y-%m-%dT%H:%M:%S.000Z")
+                updated_date = change_timezone_to_pst(datetime_object)
+                speedtest_object.update({dictPair: updated_date})
+
+
+def database_queries(filename, amount, database_connection):
+    specific_results = database_connection.cream_of_the_crop(hours=amount)
+
+    specific_json = specific_results.to_json(orient='records', date_format='iso')
+    parsed = json.loads(specific_json)
+    replace_datetime_formats(parsed)
+
+    with open(f'webpages/json data/{filename}', 'w', encoding='utf-8') as f:
+        json.dump(parsed, f, ensure_ascii=False, indent=4)
+
+
+@repeat(every(1).hour)
+def job():
+    base = DB()
+    database_queries(filename='TESTING recent one hour all location speeds.json', amount=1, database_connection=base)
+    database_queries(filename='TESTING five hours all location speeds.json', amount=5, database_connection=base)
+    base.closing()
+
+
+def main():
+    while True:
+        run_pending()
+        time.sleep(120)
+
+
+if __name__ == '__main__':
+    main()
